73805d2bdab08734cb6178827cdb1ae7bda7e23d
Daria Lapina <lapinadaria@gmail.com>
2017-05-17 21:53:08 +0300

    add gem slim-rails, config generators

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,16 @@ module Thinknetica
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
