e76376289c1fa4c23e182c23047d3e01b7f9e8cc
Daria Lapina <lapinadaria@gmail.com>
2017-05-18 16:04:25 +0300

    show, list, create questions with tests

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+  before_action :load_question, only: [:show]
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+private
+
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+end
