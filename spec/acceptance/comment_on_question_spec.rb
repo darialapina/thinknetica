be63b8c52011e2d145bcdece07b52bd19ef96a85
Daria Lapina <lapinadaria@gmail.com>
2017-07-24 22:35:21 +0300

    shared examples for comments

--- spec/acceptance/comment_on_question_spec.rb
+++ spec/acceptance/comment_on_question_spec.rb
@@ -8,74 +8,7 @@ feature 'Comment on question', %q{
 
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
+  given!(:container_class) { 'question_comments' }
 
-  scenario 'Unauthenticated user tries to add comment' do
-    visit question_path(question)
-
-    within '.question_comments' do
-      expect(page).not_to have_content 'Add a comment'
-    end
-  end
-
-  describe 'Authenticated user' do
-    before do
-      sign_in user
-      visit question_path(question)
-    end
-
-    scenario 'sees add comment form' do
-      within '.question_comments' do
-        expect(page).to have_content 'Add a comment'
-      end
-    end
-
-    scenario 'leaves a valid comment', js: true do
-      within '.question_comments' do
-        fill_in 'Comment', with: 'My comment'
-        click_on 'Create'
-
-        expect(page).to have_content 'My comment'
-        expect(page).not_to have_content "Body can't be blank"
-      end
-    end
-
-    scenario 'leaves an invalid comment', js: true do
-      within '.question_comments' do
-        fill_in 'Comment', with: ''
-        click_on 'Create'
-
-        expect(page).to have_content "Body can't be blank"
-      end
-    end
-  end
-
-  context 'mulitple sessions' do
-    scenario "comment to a question appears on another user's page", js: true do
-      Capybara.using_session('user') do
-        sign_in(user)
-        visit question_path(question)
-      end
-
-      Capybara.using_session('guest') do
-        visit question_path(question)
-      end
-
-      Capybara.using_session('user') do
-        within '.question_comments' do
-          fill_in 'Comment', with: 'My comment'
-          click_on 'Create'
-
-          expect(page).to have_content 'My comment'
-          expect(page).not_to have_content "Body can't be blank"
-        end
-      end
-
-      Capybara.using_session('guest') do
-        within '.question_comments' do
-          expect(page).to have_content 'My comment'
-        end
-      end
-    end
-  end
-
+  it_behaves_like "Commentable"
 end
